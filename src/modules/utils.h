9cffa105fcd0e4e666db213a42041b4708032ccc
Martin Androvich <martinandrovich@gmail.com>
2019-10-05 16:51:52 +0200

    Add exec_path()

--- src/modules/utils.h
+++ src/modules/utils.h
@@ -1,6 +1,20 @@
 #pragma once
 
+#include <string>
+#include <filesystem>
+
+#include <gazebo/gazebo_client.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/transport/transport.hh>
+#include <opencv2/opencv.hpp>
+#include <fl/Headers.h>
+
+#include "core-common.h"
+
+// public declarations for utils
+
 namespace utils
 {
-	
+	std::string
+	exec_path();
 }
