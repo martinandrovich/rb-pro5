e58605bdc9b22a7b5d908828c61f431fc91e2608
Martin Androvich <martinandrovich@gmail.com>
2019-11-07 22:16:32 +0100

    add line segment detection

--- src/modules/utils.h
+++ src/modules/utils.h
@@ -45,3 +45,66 @@ scale_image(cv::Mat& img, const dim_t& dim_world, const float meter_per_px)
 
 	return scale;
 }
+
+// Given three colinear points p, q, r, the function checks if 
+// point q lies on line segment 'pr' 
+inline bool
+on_segment(const cv::Point& p, const cv::Point& q, const cv::Point& r) 
+{ 
+    if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) && 
+        q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y)) 
+       return true; 
+	
+	else
+    return false; 
+} 
+
+// To find orientation of ordered triplet (p, q, r). 
+// The function returns following values 
+// 0 --> p, q and r are colinear 
+// 1 --> Clockwise 
+// 2 --> Counterclockwise 
+inline int
+orientation(const cv::Point& p, const cv::Point& q, const cv::Point& r) 
+{ 
+    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
+    // for details of below formula. 
+    int val = (q.y - p.y) * (r.x - q.x) - 
+              (q.x - p.x) * (r.y - q.y); 
+  
+    if (val == 0) return 0;  // colinear 
+  
+    return (val > 0)? 1: 2; // clock or counterclock wise 
+}
+
+// The main function that returns true if line segment 'p1q1' 
+// and 'p2q2' intersect. 
+inline bool
+do_intersect(const cv::Point& p1, const cv::Point& q1, const cv::Point& p2, const cv::Point& q2) 
+{ 
+    // Find the four orientations needed for general and 
+    // special cases 
+    int o1 = orientation(p1, q1, p2); 
+    int o2 = orientation(p1, q1, q2); 
+    int o3 = orientation(p2, q2, p1); 
+    int o4 = orientation(p2, q2, q1); 
+  
+    // General case 
+    if (o1 != o2 && o3 != o4) 
+        return true; 
+  
+    // Special Cases 
+    // p1, q1 and p2 are colinear and p2 lies on segment p1q1 
+    if (o1 == 0 && on_segment(p1, p2, q1)) return true; 
+  
+    // p1, q1 and q2 are colinear and q2 lies on segment p1q1 
+    if (o2 == 0 && on_segment(p1, q2, q1)) return true; 
+  
+    // p2, q2 and p1 are colinear and p1 lies on segment p2q2 
+    if (o3 == 0 && on_segment(p2, p1, q2)) return true; 
+  
+     // p2, q2 and q1 are colinear and q1 lies on segment p2q2 
+    if (o4 == 0 && on_segment(p2, q1, q2)) return true; 
+  
+    return false; // Doesn't fall in any of the above cases 
+} 
