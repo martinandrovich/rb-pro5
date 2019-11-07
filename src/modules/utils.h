3b22626e73501a56fce20a72a3121cf52f202328
Martin Androvich <martinandrovich@gmail.com>
2019-11-07 22:46:46 +0100

    update line segment algorithm

--- src/modules/utils.h
+++ src/modules/utils.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <stdexcept>
 
 #include <opencv2/opencv.hpp>
@@ -46,65 +47,70 @@ scale_image(cv::Mat& img, const dim_t& dim_world, const float meter_per_px)
 	return scale;
 }
 
-// Given three colinear points p, q, r, the function checks if 
-// point q lies on line segment 'pr' 
-inline bool
-on_segment(const cv::Point& p, const cv::Point& q, const cv::Point& r) 
-{ 
-    if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) && 
-        q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y)) 
-       return true; 
-	
-	else
-    return false; 
-} 
+// segment intersection
 
-// To find orientation of ordered triplet (p, q, r). 
-// The function returns following values 
-// 0 --> p, q and r are colinear 
-// 1 --> Clockwise 
-// 2 --> Counterclockwise 
-inline int
-orientation(const cv::Point& p, const cv::Point& q, const cv::Point& r) 
-{ 
-    // See https://www.geeksforgeeks.org/orientation-3-ordered-points/ 
-    // for details of below formula. 
-    int val = (q.y - p.y) * (r.x - q.x) - 
-              (q.x - p.x) * (r.y - q.y); 
-  
-    if (val == 0) return 0;  // colinear 
-  
-    return (val > 0)? 1: 2; // clock or counterclock wise 
+inline constexpr auto EPSILON = 0.000001;
+
+struct line_t
+{
+	cv::Point from;
+	cv::Point to;
+
+	std::array<cv::Point, 2>
+	bounding_box()
+	{
+		std::array<cv::Point, 2> box;
+
+		box[0] = cv::Point(std::min(from.x, to.x), std::min(from.y, to.y));
+		box[1] = cv::Point(std::max(from.x, to.x), std::max(from.y, to.y));
+
+		return box;
+	}
+};
+
+inline double
+cross(cv::Point a, cv::Point b)
+{
+	return a.x * b.y - b.x * a.y;
 }
 
-// The main function that returns true if line segment 'p1q1' 
-// and 'p2q2' intersect. 
 inline bool
-do_intersect(const cv::Point& p1, const cv::Point& q1, const cv::Point& p2, const cv::Point& q2) 
-{ 
-    // Find the four orientations needed for general and 
-    // special cases 
-    int o1 = orientation(p1, q1, p2); 
-    int o2 = orientation(p1, q1, q2); 
-    int o3 = orientation(p2, q2, p1); 
-    int o4 = orientation(p2, q2, q1); 
-  
-    // General case 
-    if (o1 != o2 && o3 != o4) 
-        return true; 
-  
-    // Special Cases 
-    // p1, q1 and p2 are colinear and p2 lies on segment p1q1 
-    if (o1 == 0 && on_segment(p1, p2, q1)) return true; 
-  
-    // p1, q1 and q2 are colinear and q2 lies on segment p1q1 
-    if (o2 == 0 && on_segment(p1, q2, q1)) return true; 
-  
-    // p2, q2 and p1 are colinear and p1 lies on segment p2q2 
-    if (o3 == 0 && on_segment(p2, p1, q2)) return true; 
-  
-     // p2, q2 and q1 are colinear and q1 lies on segment p2q2 
-    if (o4 == 0 && on_segment(p2, q1, q2)) return true; 
-  
-    return false; // Doesn't fall in any of the above cases 
-} 
+doBoundingBoxesIntersect(std::array<cv::Point, 2> a, std::array<cv::Point, 2> b)
+{
+	return a[0].x <= b[1].x && a[1].x >= b[0].x && a[0].y <= b[1].y	&& a[1].y >= b[0].y;
+}
+
+inline bool
+isPointOnLine(line_t& line, cv::Point pt)
+{
+	// move the image, so that a.first is on (0|0)
+	auto line_temp = line_t{ { 0, 0 }, cv::Point(line.to.x - line.from.x, line.to.y - line.from.y) };	
+	auto pt_temp   = cv::Point(pt.x - line.from.x, pt.y - line.from.y);
+
+	return (std::abs(cross(line_temp.to, pt_temp)) < EPSILON);
+}
+
+inline bool
+isPointRightOfLine(line_t line, cv::Point pt)
+{
+	// move the image, so that a.first is on (0|0)
+	auto line_temp = line_t{ { 0, 0 }, cv::Point(line.to.x - line.from.x, line.to.y - line.from.y) };	
+	auto pt_temp   = cv::Point(pt.x - line.from.x, pt.y - line.from.y);
+
+	return (cross(line_temp.to, pt_temp) < 0);
+}
+
+inline bool
+lineSegmentTouchesOrCrossesLine(line_t& a, line_t& b)
+{
+	return isPointOnLine(a, b.from) || isPointOnLine(a, b.to) || (isPointRightOfLine(a, b.from) ^ isPointRightOfLine(a, b.to));
+}
+
+inline bool
+segment_intersect(line_t& a, line_t& b)
+{
+	auto box1 = a.bounding_box();
+	auto box2 = b.bounding_box();
+
+	return doBoundingBoxesIntersect(box1, box2)	&& lineSegmentTouchesOrCrossesLine(a, b) && lineSegmentTouchesOrCrossesLine(b, a);
+}
