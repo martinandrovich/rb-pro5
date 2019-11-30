fc53b5ff3302931df503b6f347004d6d57fd2a63
Martin Androvich <martinandrovich@gmail.com>
2019-11-30 05:58:39 +0100

    add some helpers

--- src/modules/utils.h
+++ src/modules/utils.h
@@ -12,23 +12,40 @@
 #include "../constants.h"
 #include "types/pose.h"
 
-namespace utils
+// -- morph structuring elements  -------------------------------------------------
+
+namespace MORPH
 {
-	cv::Mat
-	load_img(const std::string& filepath, cv::ImreadModes mode = cv::IMREAD_UNCHANGED);
-
-	void
-	show_img(const std::string& name, const cv::Mat& img, bool close = true);
-
-	void
-	scale_image(cv::Mat& img, float scale);
-
-	float
-	scale_image(cv::Mat& img, const dim_t& dim_world, float meter_per_px);
-
-	template<typename D = std::chrono::microseconds, typename F> float
-	benchmark(F lambda, bool print = true);
+	inline const auto STR_ELM_RECT_2x2 = cv::getStructuringElement(cv::MORPH_RECT,    cv::Size(2, 2));
+	inline const auto STR_ELM_RECT_5x5 = cv::getStructuringElement(cv::MORPH_RECT,    cv::Size(5, 5));
+	inline const auto STR_ELM_RECT_9x9 = cv::getStructuringElement(cv::MORPH_RECT,    cv::Size(9, 9));
+	inline const auto STR_ELM_CROS_2x2 = cv::getStructuringElement(cv::MORPH_CROSS,   cv::Size(2, 2));
+	inline const auto STR_ELM_ELPS_2x2 = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(2, 2));
 }
+
+// -- pixel  ----------------------------------------------------------------------
+
+namespace PIXEL
+{
+	constexpr auto BLACK   = 0;
+	constexpr auto WHITE   = 255;
+	constexpr auto GRAY    = 127;
+
+	constexpr auto NOT_OBS = WHITE;
+	constexpr auto IS_OBS  = BLACK;
+
+	inline const auto TL = cv::Point(-1, -1);
+	inline const auto TM = cv::Point( 0, -1);
+	inline const auto TR = cv::Point( 1, -1);
+	inline const auto ML = cv::Point(-1,  0);
+	inline const auto MM = cv::Point( 0,  0);
+	inline const auto MR = cv::Point( 1,  0);
+	inline const auto BL = cv::Point(-1,  1);
+	inline const auto BM = cv::Point( 0,  1);
+	inline const auto BR = cv::Point( 1,  1);
+}
+
+// -- helper methods  -------------------------------------------------------------
 
 inline cv::Mat
 load_img(const std::string& filepath, cv::ImreadModes mode = cv::IMREAD_UNCHANGED)
@@ -65,6 +82,12 @@ scale_img(cv::Mat& img, const dim_t& dim_world, const float meter_per_px)
 	cv::resize(img, img, cv::Size(), scale_width, scale_height, cv::INTER_NEAREST);
 
 	return scale;
+}
+
+inline bool
+pt_within_boundary(const cv::Mat& img, const cv::Point& pt)
+{
+	return not ((pt.x - 1) < 0 || (pt.y - 1) < 0 || (pt.x + 1) > img.cols || (pt.y + 1) > img.rows);
 }
 
 inline void
@@ -136,6 +159,31 @@ iterate_8adj(cv::Mat& img, cv::Point pt, std::function<void(cv::Point&, uchar&)>
 	}
 }
 
+template<typename T>
+inline void
+iterate_3x3(cv::Mat& img, cv::Point pt, std::function<void(cv::Point&, T&)> callback)
+{
+	// boundary restrictions for 3x3 kernel
+	size_t bound_row, bound_col, start_col, start_row;
+
+	bound_row = (pt.y >= (img.rows - 1)) ? img.rows : pt.y + 2;
+	bound_col = (pt.x >= (img.cols - 1)) ? img.cols : pt.x + 2;
+	start_col = pt.x - ((pt.x <= 0) ? 0 : 1);
+	start_row = pt.y - ((pt.y <= 0) ? 0 : 1);
+
+	// seek 3x3 kernel grid
+	for (size_t row = start_row; row < (size_t)bound_row; row++)
+	{
+		for (size_t col = start_col; col < (size_t)bound_col; col++)
+		{
+			auto  pos   = cv::Point(col, row);
+			auto& pixel = img.at<T>(pos);
+
+			callback(pos, pixel);
+		}
+	}
+}
+
 template<typename D = std::chrono::microseconds, typename F> float
 benchmark(F lambda, bool print = true)
 {
@@ -177,25 +225,25 @@ template <typename T>
 void
 substract_vector(std::vector<T>& a, const std::vector<T>& b)
 {
-    typename std::vector<T>::iterator       it = a.begin();
-    typename std::vector<T>::const_iterator it2 = b.begin();
+	typename std::vector<T>::iterator       it = a.begin();
+	typename std::vector<T>::const_iterator it2 = b.begin();
 
-    while (it != a.end())
-    {
-        while (it2 != b.end() && it != a.end())
-        {
-            if (*it == *it2)
-            {
-                it = a.erase(it);
-                it2 = b.begin();
-            }
+	while (it != a.end())
+	{
+		while (it2 != b.end() && it != a.end())
+		{
+			if (*it == *it2)
+			{
+				it = a.erase(it);
+				it2 = b.begin();
+			}
 
-            else
-                ++it2;
-        }
-        if (it != a.end())
-            ++it;
+			else
+				++it2;
+		}
+		if (it != a.end())
+			++it;
 
-        it2 = b.begin();
-    }
+		it2 = b.begin();
+	}
 }
